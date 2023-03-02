ea6f3637c7ca5830a021330c28ef88888987f5c1
Erul - Data Enthusiast <119069839+syaerulid@users.noreply.github.com>
2023-03-02 18:31:49 +0700

    Add files via upload

---
+++ data/1.7. Using + for String Concatenation in Python.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# - Using + sign for String Concatenation
+#       - + can only Concatenate String text with another String text
+#       - + cannot be used to contatenate String text with a number
+#     
+# - Using, sign in print statements when concateting string text with numbers
+
+# In[1]:
+
+
+print("Syaerul" + " " + "Rochman") # Concatenation Operator
+
+
+# In[2]:
+
+
+print(5+4) # Addition Operator
+
+
+# In[3]:
+
+
+print("Syaerul" + 9) # Error
+
+
+# In[4]:
+
+
+print("Syaerul" + str(9)) # not error
+
+
+# In[5]:
+
+
+print('Syaerul', 9) # not error but get spaces
+
+
+# In[ ]:
+
+
+
+
