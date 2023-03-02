e989b9b0549d2a3dd1ffbd0bdfa02da6426bb98b
Erul - Data Enthusiast <119069839+syaerulid@users.noreply.github.com>
2023-03-02 22:16:59 +0700

    Add files via upload

---
+++ data/2.2. Repetition or Loops in Python (While).py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# - For Loops
+# - While Loops
+
+# In[7]:
+
+
+i = 1
+
+
+# In[ ]:
+
+
+while i <= 10:
+    print(i) # wiull print infinite loops of i or 1 1 1 1 1 1
+
+
+# In[8]:
+
+
+while i <= 10:
+    print(i) # will print 1 
+    i+=1 # 2
+
+
+# In[9]:
+
+
+print("End of the program")
+
+
+# In[10]:
+
+
+# +Else
+
+while i <= 10:
+    print(i) # will print 1 
+    i+=1 # 2
+    
+else :
+    print("Came out of while loop when the value of i have become:", i)
+
+
+# In[ ]:
+
+
+
+
