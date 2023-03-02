ac17da38c6ac5c12b82e2bdc73b23ea0039e56df
Erul - Data Enthusiast <119069839+syaerulid@users.noreply.github.com>
2023-03-02 20:38:07 +0700

    Add files via upload

---
+++ data/2. Control Flow Statements in Python.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# - Selection / Decision Control statements
+#     - if
+#     - if .. else
+#     - if .. elif .. else
+# - Repetition / Loop Statements
+#     - while loop
+#     - for loop
+# - Transfer / Jump Statements
+#     - break
+#     - continue
+#     - return
+#     - try & except
+
+# In[ ]:
+
+
+
+
