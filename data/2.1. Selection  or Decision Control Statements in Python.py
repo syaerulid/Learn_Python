16005f77f3f43c355e56d3a7849e0dafb62ae96a
Erul - Data Enthusiast <119069839+syaerulid@users.noreply.github.com>
2023-03-02 20:57:23 +0700

    Add files via upload

---
+++ data/2.1. Selection  or Decision Control Statements in Python.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ## Selection / Decision Control Statements
+
+# - If
+# - If .. Else
+# - Elif
+
+# # If
+
+# In[1]:
+
+
+a = 5
+b = 4
+
+
+# In[2]:
+
+
+if a > b:
+    print("a is greater than b")
+
+
+# In[3]:
+
+
+a,b,c,d,e = 5,4,3,2,1
+
+
+# In[5]:
+
+
+"""in this program below, they will check by conditions, one by one, and if conditions = true, the loops is end."""
+
+if a < b:
+    print("a is less than b")
+elif b < c:
+    print("b is less than c")
+elif c < d :
+    print("c is less than d")
+elif d < e:
+    print("d is less than e")
+else:
+    print("e is the least")
+
+
+# In[ ]:
+
+
+
+
