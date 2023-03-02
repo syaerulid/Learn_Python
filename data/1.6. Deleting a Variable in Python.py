ead6d99bcb2ba1024520aac10603c5cab09f5388
Erul - Data Enthusiast <119069839+syaerulid@users.noreply.github.com>
2023-03-02 18:27:35 +0700

    Add files via upload

---
+++ data/1.6. Deleting a Variable in Python.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# - del variable_name
+
+# In[3]:
+
+
+a = 11
+print(a)
+
+
+# In[4]:
+
+
+del a
+
+
+# In[5]:
+
+
+print(a) # not defined means a doesn't store any value because already deleted
+
+
+# In[ ]:
+
+
+
+
