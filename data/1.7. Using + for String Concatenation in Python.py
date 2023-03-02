e098be56cf608d8729dac8cdbfc2b7c504b15d4b
Erul - Data Enthusiast <119069839+syaerulid@users.noreply.github.com>
2023-03-02 18:27:54 +0700

    Add files via upload

---
+++ data/1.7. Using + for String Concatenation in Python.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+print("Syaerul" + " " + "Rochman") # Concatenation Operator
+
+
+# In[2]:
+
+
+print(5+4) # Addition Operator
+
+
+# In[3]:
+
+
+print("Syaerul" + 9) # Error
+
+
+# In[4]:
+
+
+print("Syaerul" + str(9)) # not error
+
+
+# In[5]:
+
+
+print('Syaerul', 9) # not error but get spaces
+
+
+# In[ ]:
+
+
+
+
