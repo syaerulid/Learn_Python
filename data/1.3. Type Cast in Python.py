724df5678c1beafa1f9f50b1f531ff8a87741280
Erul - Data Enthusiast <119069839+syaerulid@users.noreply.github.com>
2023-03-02 17:41:29 +0700

    Add files via upload

---
+++ data/1.3. Type Cast in Python.py
@@ -0,0 +1,104 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ## Float to Int
+
+# In[1]:
+
+
+a = 1.25
+
+
+# In[2]:
+
+
+print(type(a))
+print(a)
+
+
+# In[3]:
+
+
+a = int(1.25)
+
+
+# In[4]:
+
+
+print(type(a))
+print(a)
+
+
+# ## String to float
+
+# In[5]:
+
+
+a = ("9")
+
+
+# In[6]:
+
+
+print(type(a))
+print(a)
+
+
+# In[7]:
+
+
+print(a+1) # not possible because still string
+
+
+# In[8]:
+
+
+a = int("9")
+
+
+# In[9]:
+
+
+print(type(a))
+print(a+1)
+
+
+# ## Float to string
+
+# In[10]:
+
+
+a = 9.5
+
+
+# In[11]:
+
+
+print(type(a))
+print(a)
+
+
+# In[14]:
+
+
+a = str(9)
+
+
+# In[15]:
+
+
+print(type(a))
+print(a +" is a number string")
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
