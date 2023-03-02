922ca6898809faab2c08da70e6c5948091758195
Erul - Data Enthusiast <119069839+syaerulid@users.noreply.github.com>
2023-03-02 17:32:20 +0700

    Add files via upload

---
+++ data/1.2. Data Types in Python.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Data Type
+#    - int
+#    - float
+#    - string
+#    - boolean
+#    - List
+#    - Tuple
+#    - Dictionary
+#     - using type() for finding the Data Type of the variable in Python
+
+# In[1]:
+
+
+a = 8
+print(a)
+
+
+# In[2]:
+
+
+type(a)
+
+
+# In[3]:
+
+
+b = "Syaerul Rochman"
+print(b)
+
+
+# In[4]:
+
+
+type(b)
+
+
+# In[5]:
+
+
+print(type(b))
+
+
+# In[6]:
+
+
+abc = [1,2,3]
+
+
+# In[7]:
+
+
+print(abc)
+
+
+# In[8]:
+
+
+type(abc)
+
+
+# In[ ]:
+
+
+
+
