b84b0c0a06f60787bf17398ff3d8dac26611fc41
Erul - Data Enthusiast <119069839+syaerulid@users.noreply.github.com>
2023-03-02 23:40:02 +0700

    Add files via upload

---
+++ data/2.3. Repetition or Loops in Python (for).py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+for i in range(10):
+    print(i) # python index start from 0
+
+
+# In[3]:
+
+
+for i in range(4, 15):
+    print(i) # python index start from 4 to 14
+
+
+# In[4]:
+
+
+for i in range(2,11,3):
+    print(i) # kelipatan +3 dari 2
+
+
+# In[5]:
+
+
+for i in range(10,1,-1):
+    print(i) # indexnya dibalik dan dimulai dari index ke - 1 yaitu [2]
+
+
+# In[8]:
+
+
+for i in range(10,0,-1):
+    print(i) # indexnya dibalik dan dimulai dari index ke - 0 yaitu [1]
+
+
+# In[10]:
+
+
+for i in range(10,0,-1):
+    print(i) # indexnya dibalik dan dimulai dari index ke - 0 yaitu [1]
+else:
+    print("Inside Else Block")
+    
+print("End of this program")
+
+
+# In[ ]:
+
+
+
+
