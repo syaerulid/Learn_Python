3fe6a20a4e1007173da0584b0391d67e1d162cc2
Erul - Data Enthusiast <119069839+syaerulid@users.noreply.github.com>
2023-03-02 17:15:32 +0700

    Add files via upload

---
+++ data/1.Printing in Python Using Print Statement.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ## Print Text
+
+# In[1]:
+
+
+print("Syaerul Rochman")
+
+
+# In[4]:
+
+
+print("My Name is Syaerul Rochman")
+print("I'am currently Learning About")
+print("Python Right Now")
+
+
+# ## Print Number
+
+# In[3]:
+
+
+print(9)
+
+
+# ## Printing in 1 One Line
+
+# In[8]:
+
+
+print(1, end =" ") ## use =""
+print(2, end =" ")
+print(3, end =" ")
+
+
+# In[ ]:
+
+
+
+
