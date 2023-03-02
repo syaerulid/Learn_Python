6c5dcd90a36df4e6de8b9a2c115f5e3dd0731c61
Erul - Data Enthusiast <119069839+syaerulid@users.noreply.github.com>
2023-03-02 19:00:38 +0700

    Add files via upload

---
+++ data/1.8. Storing Multi-Line Preformatted String text into a Variable in Python.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[5]:
+
+
+p = """
+Nama saya Cholifah Turizah, saya seorang Junior Auditor di salah satu KAP Big 10 di Indonesia yaitu Crowe Indonesia.
+Saya Dulunya berkuliah di Unnes dan jurusan saya adalah Ekonomi, Saya Lulus tahun 2022 dan resmi memperoleh gelar S.E.
+pada November 2022"""
+
+
+# In[6]:
+
+
+print(p)
+
+
+# In[ ]:
+
+
+
+
