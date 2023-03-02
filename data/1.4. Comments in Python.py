f8feef0728a21134ea8b582662176791cc9e6e77
Erul - Data Enthusiast <119069839+syaerulid@users.noreply.github.com>
2023-03-02 17:48:50 +0700

    Add files via upload

---
+++ data/1.4. Comments in Python.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Single Line Comments in Python Using '#'
+
+# In[1]:
+
+
+# The below code stores 5 and 4 into variables a and b, performs the addition and then prints the result
+a = 5
+b = 4
+sum = a+b
+print(sum)
+print(sum+10)
+print(sum+21)
+
+
+# # Multiple Line Comments in Python Using "
+
+# In[2]:
+
+
+"""
+My Name is Syaerul, I'm learn about python and how to use operator in Python Right now.
+I try to use sum and print function from the stored variables below
+"""
+
+c = 7
+d = 8
+sum = c+d
+print(sum)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
