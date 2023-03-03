dd5a491fee2604cefa070b08d65f74df1c0726e2
Erul - Data Enthusiast <119069839+syaerulid@users.noreply.github.com>
2023-03-03 17:41:14 +0700

    Add files via upload

---
+++ data/3.3. Pass Statement in Python.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ## Pass Statement in Python
+#    - If the function or loop or condition block in Python program is not implemented yet and will be implemented later,
+#    we simply add pass under it for future implementation
+#    - When we execute the functions having pass under a function or loop, nothing happens
+
+# In[1]:
+
+
+# Examples:
+def sum():
+    pass
+    
+def sub():
+    pass
+    
+def mul():
+    pass
+
+def div():
+    pass
+
+
+# In[2]:
+
+
+sum()
+
+
+# In[3]:
+
+
+for i in range(1,11):
+    pass
+
+
+# In[4]:
+
+
+a = 5
+b = 4
+
+if a > b:
+    pass
+
+
+# In[ ]:
+
+
+
+
