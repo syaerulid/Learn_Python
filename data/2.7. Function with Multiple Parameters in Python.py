498580d6299d38b73321c001b47a4c736fa9f3b9
Erul - Data Enthusiast <119069839+syaerulid@users.noreply.github.com>
2023-03-03 11:37:43 +0700

    Add files via upload

---
+++ data/2.7. Function with Multiple Parameters in Python.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Multiple Parameters
+
+# In[1]:
+
+
+def sum(a,b,c,d):
+    print(a+b+c+d)
+
+
+# In[4]:
+
+
+sum(1,4,5,7)
+
+
+# # Function can return the data
+
+# In[5]:
+
+
+def sum(a,b):
+    return a+b
+
+
+# In[9]:
+
+
+sum(5,4)
+
+
+# In[7]:
+
+
+sum(7,8)
+
+
+# In[10]:
+
+
+c = sum(5,4)
+
+
+# In[11]:
+
+
+print(c)
+
+
+# In[12]:
+
+
+c
+
+
+# In[ ]:
+
+
+
+
