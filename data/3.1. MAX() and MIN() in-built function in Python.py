c948fc279274d673809702586c1860612a085474
Erul - Data Enthusiast <119069839+syaerulid@users.noreply.github.com>
2023-03-03 17:22:01 +0700

    Add files via upload

---
+++ data/3.1. MAX() and MIN() in-built function in Python.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+ma = max(8,1,5,7,11,3,8)
+
+
+# In[4]:
+
+
+print(ma)
+
+
+# In[5]:
+
+
+ma
+
+
+# In[7]:
+
+
+mi = min("orange","green","yellow","blue") # blue will came first, alphabetical
+
+
+# In[8]:
+
+
+print(mi)
+
+
+# In[ ]:
+
+
+
+
