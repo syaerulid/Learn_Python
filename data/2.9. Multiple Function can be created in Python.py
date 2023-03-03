1779269847e2f74756bf44735ba3a46de7f23f11
Erul - Data Enthusiast <119069839+syaerulid@users.noreply.github.com>
2023-03-03 17:15:12 +0700

    Add files via upload

---
+++ data/2.9. Multiple Function can be created in Python.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# - We can create multiple functions in the same python file
+
+# In[1]:
+
+
+def sum(a,b):
+    print(a+b)
+
+def sub(a,b):
+    print(a-b)
+
+def mul(a,b):
+    print(a*b)
+    
+def div(a,b):
+    print(a/b)
+
+
+# In[2]:
+
+
+sum(5,4)
+
+
+# In[3]:
+
+
+mul(3,8)
+
+
+# In[4]:
+
+
+div(7,2)
+
+
+# In[5]:
+
+
+sub(7,10)
+
+
+# In[ ]:
+
+
+
+
