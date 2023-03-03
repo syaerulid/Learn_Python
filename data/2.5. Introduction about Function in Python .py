99cabc3ef77981bfc6f02f6588c379a7c77d7a2a
Erul - Data Enthusiast <119069839+syaerulid@users.noreply.github.com>
2023-03-03 11:18:52 +0700

    Add files via upload

---
+++ data/2.5. Introduction about Function in Python .py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # What is Function?
+
+# - Function is a block of code
+# - def my_first_function():
+# - Calling function to execute the block of code
+
+# In[1]:
+
+
+def my_first_function():
+    print("Inside my first function")
+
+
+# In[2]:
+
+
+# calling the function
+
+my_first_function()
+
+
+# In[3]:
+
+
+# Function can be called multiple times
+
+my_first_function()
+
+
+# In[4]:
+
+
+my_first_function()
+
+
+# In[5]:
+
+
+my_first_function()
+my_first_function()
+
+
+# In[ ]:
+
+
+
+
