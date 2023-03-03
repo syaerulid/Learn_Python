77824f1a3e4aaea6e14aad61d75d88b4cccf84d4
Erul - Data Enthusiast <119069839+syaerulid@users.noreply.github.com>
2023-03-03 17:52:07 +0700

    Add files via upload

---
+++ data/3.4. Collections in Python.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Collections in Python
+#    - Collection Data Types - List, Tuple, Set and Dictionary
+
+# Collections is
+# - Data Types
+# which allow us to store multiple values into a single variable
+
+# 4 Collection Data Types in Python :
+#    - List
+#    - Tuple
+#    - Set
+#    - Dictionary
+
+# In[2]:
+
+
+# Example storing multiple values
+colors = ["orange","green","yellow","red","white"]
+
+
+# In[3]:
+
+
+print(type(colors))
+
+
+# In[ ]:
+
+
+
+
