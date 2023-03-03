38b7f7f83891ae80f9624657aaf33bf94ac3a5b4
Erul - Data Enthusiast <119069839+syaerulid@users.noreply.github.com>
2023-03-03 11:42:32 +0700

    Add files via upload

---
+++ data/2.8. Purpose of Functions.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Purpose :
+#    - Repetition of the code can be resolved using functions
+
+# # Repetition
+
+# In[1]:
+
+
+a = 5
+b = 4
+sum = a+b
+print(sum)
+
+
+# In[2]:
+
+
+c = 3
+d = 5
+sum = c+d
+print(sum)
+
+
+# In[3]:
+
+
+e = 2
+f = 4
+sum = e+f
+print(sum)
+
+
+# # Use Function so its more eficient
+
+# In[4]:
+
+
+def sum(a,b):
+    s = a+b
+    print(s)
+
+
+# In[5]:
+
+
+sum(2,4)
+
+
+# In[ ]:
+
+
+
+
